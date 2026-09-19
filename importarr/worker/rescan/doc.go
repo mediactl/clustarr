782b4520d6b5a172fbf34f75c92a5948ba664b87
appkins <nbatkins@gmail.com>
2026-09-18 20:07:23 -0500

    style(rescan): gofmt doc.go after the registration-example fix

--- importarr/worker/rescan/doc.go
+++ importarr/worker/rescan/doc.go
@@ -63,6 +63,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 // k8s.EveryReplica, not manager.RunnableFunc: the latter has no
 // NeedLeaderElection method, so controller-runtime puts it behind the leader
 // lease on any service that elects.
+//
 //	if err := mgr.Add(k8s.EveryReplica(func(ctx context.Context) error {
 //	        stop, err := bus.Subscribe(ctx, spec.Subscription(), worker.Handle)
 //	        if err != nil {
